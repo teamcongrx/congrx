efb25f52fc9d4dc03ca2a9b8eec052f211e8b9a9
teamcongrx <teamcongrx@gmail.com>
2026-03-29 19:13:41 -0400

    Update page.tsx

--- congrx_deploy/app/page.tsx
+++ congrx_deploy/app/page.tsx
@@ -1,4 +1,11 @@
 /* eslint-disable */
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
-  return null
+  return (
+    <iframe 
+      src="/app.html" 
+      style={{width:'100%', height:'100vh', border:'none'}}
+    />
+  )
 }
