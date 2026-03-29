6d1b720c2fe109fac45d2c6b9e1f0f99e332be26
teamcongrx <teamcongrx@gmail.com>
2026-03-29 19:09:05 -0400

    Update page.tsx

--- congrx_deploy/app/page.tsx
+++ congrx_deploy/app/page.tsx
@@ -1,11 +1,4 @@
 /* eslint-disable */
 export default function Home() {
-  return (
-    <html>
-      <head>
-        <meta httpEquiv="refresh" content="0; url=/app.html" />
-      </head>
-      <body></body>
-    </html>
-  )
+  return null
 }
