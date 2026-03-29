19536f67cfe0795d8e2f86c57f363944e0d1fa24
teamcongrx <teamcongrx@gmail.com>
2026-03-29 19:16:26 -0400

    Update next.config.js

--- congrx_deploy/next.config.js
+++ congrx_deploy/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
