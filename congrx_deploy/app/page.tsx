a6d471d7782df9dea71c15655d3a0db51d034422
teamcongrx <teamcongrx@gmail.com>
2026-03-30 09:02:56 -0400

    Update page.tsx

--- congrx_deploy/app/page.tsx
+++ congrx_deploy/app/page.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable */
 export default function Home() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="refresh" content="0; url=/site.html" />
-      </head>
-      <body></body>
-    </html>
+    <script dangerouslySetInnerHTML={{__html: `window.location.replace('/site.html')`}} />
   )
 }
