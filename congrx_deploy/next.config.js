1722c2c27ad23acf45783ef3c4f130f22b21985f
teamcongrx <teamcongrx@gmail.com>
2026-03-29 20:44:33 -0400

    Update next.config.js

--- congrx_deploy/next.config.js
+++ congrx_deploy/next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
