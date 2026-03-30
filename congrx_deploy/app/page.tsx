2ffab92ae7e0fbc90d8e19c25937677aa8d19bfc
teamcongrx <teamcongrx@gmail.com>
2026-03-29 20:51:08 -0400

    Update page.tsx

--- congrx_deploy/app/page.tsx
+++ congrx_deploy/app/page.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable */
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <html>
       <head>
-        <meta httpEquiv="refresh" content="0; url=/index.html" />
+        <meta httpEquiv="refresh" content="0; url=/app.html" />
       </head>
       <body></body>
     </html>
