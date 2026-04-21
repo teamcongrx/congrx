c3d1d45c7d41dfbaa203dbcf5d702568247ade96
teamcongrx <teamcongrx@gmail.com>
2026-04-21 18:20:38 -0400

    Add scripts/scrape.js in congrx_deploy

---
+++ congrx_deploy/scripts/scrape.js
@@ -0,0 +1,11 @@
+const { batchScrape } = require('../lib/scraper');
+
+batchScrape(undefined, 5)
+  .then(n => {
+    console.log('Scraped:', n, 'tweets');
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error('Scrape failed:', e.message);
+    process.exit(1);
+  });
